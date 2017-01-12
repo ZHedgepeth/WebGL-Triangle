406531d4d605e16f989e7d5cfecf9f3976420c25
zach <zhedgepeth1124@gmail.com>
2017-01-12 08:22:39 -0500

    replace triangle vertices with box vertices

--- app.js
+++ app.js
@@ -88,17 +88,81 @@ var InitDemo = function () {
   }
 
   //Create Buffer
-  var triangleVerticies =
-  [//X    Y    Z           R    G    B
-    0.0, 0.5, 0.0,       1.0, 1.0, 0.0,
-    -0.5, -0.5, 0.0,     0.5, 0.0, 1.0,
-    0.5, -0.5, 0.0,      0.1, 1.0, 0.9
-  ];
+  var boxVertices =
+	[ // X, Y, Z           R, G, B
+		// Top
+		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
 
-  var triangVertexBufferObject = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangVertexBufferObject);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerticies), gl.STATIC_DRAW);
+		// Left
+		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+		// Right
+		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+		// Front
+		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+		// Back
+		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+		// Bottom
+		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
+	];
+
+
+  	var boxIndices =
+  	[
+  		// Top
+  		0, 1, 2,
+  		0, 2, 3,
+
+  		// Left
+  		5, 4, 6,
+  		6, 4, 7,
+
+  		// Right
+  		8, 9, 10,
+  		8, 10, 11,
+
+  		// Front
+  		13, 12, 14,
+  		15, 14, 12,
+
+  		// Back
+  		16, 17, 18,
+  		16, 18, 19,
+
+  		// Bottom
+  		21, 20, 22,
+  		22, 20, 23
+  	];
+
+  var boxVertexBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
   //STATIC_DRAW is sending the memory from the cpu to gpu once
+
+  var boxIndexBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
   var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
   var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
@@ -134,7 +198,7 @@ var InitDemo = function () {
   var viewMatrix = new Float32Array(16);
   var projMatrix = new Float32Array(16);
   mat4.identity(worldMatrix);
-  mat4.lookAt(viewMatrix, [0, 0, -2], [0, 0, 0], [0, 1, 0]);//make a camera, lookAt takes 3 parameters 3d vector for positon of viewer -5 in this instance is 5 units away try -2 and see what happens, where they're looking at, and up
+  mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);//make a camera, lookAt takes 3 parameters 3d vector for positon of viewer -5 in this instance is 5 units away try -2 and see what happens, where they're looking at, and up
   mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width/ canvas.height, 0.1, 1000.0);
 
 //Now time to send these matrices to the shader
@@ -156,7 +220,7 @@ var InitDemo = function () {
 
       gl.clearColor(0.75, 0.85, 0.8, 1.0);
       gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-      gl.drawArrays(gl.TRIANGLES, 0, 3);
+      gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT,  0);
 
       requestAnimationFrame(loop);
   };
